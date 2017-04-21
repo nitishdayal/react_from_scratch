6aa6811ea5a9cbb72a6850e7732b61097605a6b7
Nitish Dayal <nitishdayal@outlook.com>
2017-04-21 04:57:28 -0700

    Remove extraneous code What purpose that function was serving is beyond me. Apparently it seemed logical a few hours ago. Not it's like...wat.

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-const App = (props: HelloProps) => <h1>{getMessage(props)}</h1>
-
-const getMessage =
-  ({ bundler, compiler, framework }: HelloProps) =>
-    `Hello from ${framework}, running on ${compiler} and bundled with ${bundler}!`;
+const App = ({ bundler, compiler, framework }: HelloProps) =>
+  <h1>Hello from {framework}, running on {compiler} and bundled with {bundler}!</h1>;
 
 export default App;
