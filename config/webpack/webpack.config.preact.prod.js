492f7e69a52b87c971b7434e21e086b44770f0a7
Nitish Dayal <nitishdayal@outlook.com>
2017-04-24 04:33:59 -0700

    Trying to implement preact-hmr, currently bug-tastic

--- config/webpack/webpack.config.preact.prod.js
+++ config/webpack/webpack.config.preact.prod.js
@@ -1,10 +1,19 @@
+const webpack = require('webpack')
+
 module.exports = env => {
-  const conf = require('../webpack.config.prod')(env)
+  const conf = require('./webpack.config.dev')(env)
 
   conf.resolve.alias = {
     'react': 'preact-compat',
     'react-dom': 'preact-compat'
   }
+
+  conf.plugins = [
+    ...conf.plugins,
+    new webpack.DefinePlugin({
+      'preact': true
+    })
+  ]
 
   return conf
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,47 @@
 import { normalize } from 'polished';
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import { injectGlobal } from 'styled-components';
 import Routes from './containers/routes';
 
-function init() {
+let rootRender;
+
+rootRender = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('content')
+  );
+};
+
+const init = () => {
   // tslint:disable
   injectGlobal`${normalize()}`
   // tslint:enable
-  render(<Routes />, document.getElementById('content'));
-}
+
+  if (process.env.preact) {
+    console.log('PREACT BB');
+    const render = require('preact').render;
+
+    if (process.env.NODE_ENV !== 'production') {
+      require('preact/devtools');
+    }
+
+    rootRender = (Component) => {
+      render(
+        <AppContainer>
+          <Routes />
+        </AppContainer>,
+        document.body,
+        rootRender);
+    };
+  }
+
+  rootRender(Routes);
+
+};
 
 init();
 
