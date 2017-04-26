5cf5b8b57bcc4e080d20de4edf946a95fc616fc3
Nitish Dayal <nitishdayal@outlook.com>
2017-04-25 18:03:10 -0700

    MobX integrated! Oh my god that was too easy

--- src/components/page2.tsx
+++ src/components/page2.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Page2 = () => (
-  <div>
-    <h1>Welcome to Page 2</h1>
-    <ul>
-      <li>It's</li>
-      <li>an</li>
-      <li>unordered</li>
-      <li>list!</li>
-    </ul>
-    <Link to={`/`}>Back</Link>
-  </div>
-);
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+
+
+
+const listItems = observable(['first item', 'second item', 'third item'])
+
+@observer class Page2 extends Component<{}, {}> {
+
+
+  addItem = () => listItems.push('New item!')
+
+  render() {
+
+    const displayList = listItems.map((li, i) => (<li key={i}>{li}</li>))
+
+    return (
+      <div>
+        <button onClick={this.addItem}>Add an item!</button>
+        <ul>
+          {displayList}
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default Page2;
